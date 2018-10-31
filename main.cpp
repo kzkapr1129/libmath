708b6713ef3930b27a20838f30dcbb0c401e7e11
中山一輝 <nakayama@MacBook-Home.local>
2018-10-31 09:25:32 +0900

    行列のクラス化

--- main.cpp
+++ main.cpp
@@ -6,98 +6,34 @@
 //  Copyright © 2018年 中山一輝. All rights reserved.
 //
 
-#include <stdint.h>
-#include <stdio.h>
-#include <math.h>
-#include <time.h>
-#include <chrono>
-#include <iostream>
-
-template<int COLS>
-void dispM(const int M[4][COLS]) {
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < COLS; j++) {
-            printf("%d ", M[i][j]);
-        }
-        printf("\n");
-    }
-}
-
-template<int COLS>
-void dispM(const float M[4][COLS]) {
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < COLS; j++) {
-            printf("%f ", M[i][j]);
-        }
-        printf("\n");
-    }
-}
-
-void dispV(const int V[4]) {
-    for (int i = 0; i < 4; i++) {
-        printf("%d ", V[i]);
-    }
-    printf("\n");
-}
-
-void dispV(const float V[4]) {
-    for (int i = 0; i < 4; i++) {
-        printf("%f ", V[i]);
-    }
-    printf("\n");
-}
-
-
-template<typename TYPE, int AW, int BW>
-void mulMM(const TYPE a[4][AW], const TYPE b[4][BW], TYPE c[4][BW]) {
-#ifdef DEBUG
-    if (AW < BW || AW > 4) {
-        std::cout << "WARN: AW: " << AW << ", BW:" << BW << std::endl;
-        return;
-    }
-#endif
-    constexpr size_t SIZE = sizeof(TYPE) * 4 * BW;
-    memset(c, 0, SIZE);
-    
-    for (int i = 0; i < 4; i++) {
-        for (int k = 0; k < AW; k++) {
-            for (int j = 0; j < BW; j++) {
-                c[i][j] += a[i][k] * b[k][j];
-            }
-        }
-    }
-}
-
-template<typename TYPE, int AW>
-void mulMV(const TYPE a[4][AW], const TYPE b[4], TYPE c[4]) {
-#ifdef DEBUG
-    if (AW > 4) {
-        std::cout << "WARN: AW: " << AW << std::endl;
-        return;
-    }
-#endif
-    constexpr size_t SIZE = sizeof(TYPE) * 4;
-    memset(c, 0, SIZE);
-    
-    for (int i = 0; i < 4; i++) {
-        for (int k = 0; k < AW; k++) {
-            c[i] += a[i][k] * b[k];
-        }
-    }
-}
+#include "matrix.h"
 
 int main(int argc, const char * argv[]) {
-    float a[4][4] = {{2, 3, 8, 6}, {1, 4, 8, 5}, {2, 1, 4, 3}, {8, 8, 6, 3}};
-    float b[4][4] = {{3, 6, 0, 0}, {6, 4, 0, 0}, {7, 2, 0, 0}, {6, 3, 0, 0}};
-    float c[4][4];
-    mulMM(a, b, c);
-    dispM(c);
-    
-    float d[4][4] = {{2, 3, 8, 6}, {1, 4, 8, 5}, {2, 1, 4, 3}, {8, 8, 6, 3}};
-    float e[4]= {3, 6, 7, 6};
-    float f[4];
-    mulMV(d, e, f);
-    dispV(f);
-    
+    Matrix<float, 3, 3> m = {
+             1,2,1,
+             2,5,6,
+             1,3,4
+        };
+
+    Matrix<float, 3, 3> inv;
+    if (m.inverse(inv)) {
+        Matrix<float, 3, 3> mi;
+        m.mulMM(mi, inv);
+
+        std::cout << "m:" << std::endl;
+        m.disp();
+
+        std::cout << "inv:" << std::endl;
+        inv.disp();
+
+        std::cout << "m*inv:" << std::endl;
+        mi.disp();
+
+        std::cout << "identity: " << std::endl;
+        m.identity();
+        m.disp();
+    } else {
+        std::cout << "inverse matrix didn't exist" << std::endl;
+    }
     return 0;
 }
---
+++ matrix.h
@@ -0,0 +1,255 @@
+//
+//  matrix.h
+//  libmath
+//
+//  Created by 中山一輝 on 2018/10/31.
+//  Copyright © 2018年 中山一輝. All rights reserved.
+//
+
+#pragma once
+
+// INCLUDE
+#include <iostream>
+#include <initializer_list>
+
+// struct Matrix
+template<typename TYPE, int COLS, int ROWS>
+struct Matrix {
+    TYPE values[ROWS][COLS];
+    
+    Matrix();
+    Matrix(const Matrix<TYPE, COLS, ROWS>& m);
+    Matrix(std::initializer_list<TYPE> init);
+
+    inline int __attribute__((always_inline)) cols() const;
+    inline int __attribute__((always_inline)) rows() const;
+    inline void __attribute__((always_inline)) zeros();
+    void identity();
+    template<int COLS_B>
+    void mulMM(Matrix<TYPE, COLS_B, ROWS>& ret, const Matrix<TYPE, COLS_B, COLS>& m) const;
+    void transpose(Matrix<TYPE, ROWS, COLS>& ret);
+    bool inverse(Matrix<TYPE, COLS, ROWS>& inv_mat);
+    void disp() const;
+    void cofNNMatrix(Matrix<TYPE, COLS-1, ROWS-1>& ret, int x, int y) const;
+    TYPE cofactor() const;
+    const Matrix<TYPE, COLS, ROWS>& operator *= (float k);
+};
+
+/**
+ * デフォルトコンストラクタ
+ */
+template<typename TYPE, int COLS, int ROWS>
+Matrix<TYPE, COLS, ROWS>::Matrix() {
+}
+
+/**
+ * コピーコンストラクタ
+ */
+template<typename TYPE, int COLS, int ROWS>
+Matrix<TYPE, COLS, ROWS>::Matrix(const Matrix<TYPE, COLS, ROWS>& m) {
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            values[i][j] = m.values[i][j];
+        }
+    }
+}
+
+/**
+ * 初期化子を使用したコンストラクタ
+ */
+template<typename TYPE, int COLS, int ROWS>
+Matrix<TYPE, COLS, ROWS>::Matrix(std::initializer_list<TYPE> init) {
+    const TYPE* init_array = init.begin();
+    const TYPE* init_array_e = init.end();
+    
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            if (init_array != init_array_e) {
+                values[i][j] = *(init_array++);
+            } else {
+                values[i][j] = 0;
+            }
+        }
+    }
+}
+
+/**
+ * 行の大きさを返却する
+ */
+template<typename TYPE, int COLS, int ROWS>
+inline int __attribute__((always_inline)) Matrix<TYPE, COLS, ROWS>::cols() const {
+    return COLS;
+}
+
+/**
+ * 列の大きさを返却する
+ */
+template<typename TYPE, int COLS, int ROWS>
+inline int __attribute__((always_inline)) Matrix<TYPE, COLS, ROWS>::rows() const {
+    return ROWS;
+}
+
+/**
+ * 行列の全要素を0にする
+ */
+template<typename TYPE, int COLS, int ROWS>
+inline void __attribute__((always_inline)) Matrix<TYPE, COLS, ROWS>::zeros() {
+    constexpr size_t SIZE = sizeof(TYPE) * COLS * ROWS;
+    memset(values, 0, SIZE);
+}
+
+/**
+ * 単位行列を作成する
+ */
+template<typename TYPE, int COLS, int ROWS>
+void Matrix<TYPE, COLS, ROWS>::identity() {
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            values[i][j] = (i == j);
+        }
+    }
+}
+
+/**
+ * 行列同士の乗算を行う
+ * 返却される行列(ret)の大きさは乗算し合う行列の行と列を組み合わせたものとなる。
+ * this<TYPE, S, R1> * m<TYPE, C2, S> = ret<TYPE, C2, R1>
+ */
+template<typename TYPE, int COLS, int ROWS>
+template<int COLS_B>
+void Matrix<TYPE, COLS, ROWS>::mulMM(Matrix<TYPE, COLS_B, ROWS>& ret, const Matrix<TYPE, COLS_B, COLS>& m) const {
+    const int COLS_ROWS_B = COLS;
+    
+    ret.zeros();
+    
+    for (int i = 0; i < ROWS; i++) {
+        for (int k = 0; k < COLS_ROWS_B; k++) {
+            for (int j = 0; j < COLS_B; j++) {
+                ret.values[i][j] += values[i][k] * m.values[k][j];
+            }
+        }
+    }
+}
+
+/**
+ * 転置行列の返却
+ */
+template<typename TYPE, int COLS, int ROWS>
+void Matrix<TYPE, COLS, ROWS>::transpose(Matrix<TYPE, ROWS, COLS>& ret) {
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            ret.values[j][i] = values[i][j];
+        }
+    }
+}
+
+/**
+ * 逆行列を返却する
+ * @return 逆行列が存在しない場合はfalse, 存在する場合はtrue
+ */
+template<typename TYPE, int COLS, int ROWS>
+bool Matrix<TYPE, COLS, ROWS>::inverse(Matrix<TYPE, COLS, ROWS>& inv_mat) {
+    const float cof = cofactor();
+    if (cof == 0 || COLS != ROWS) {
+        // 逆行列が存在しない場合
+        return false;
+    }
+    
+    Matrix<TYPE, COLS-1, ROWS-1> cofnnm;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            // 余因子行列の取得
+            cofNNMatrix(cofnnm, j, i);
+            
+            // 符号の割り当て
+            if (((i + j) % 2) == 0) {
+                // 偶数の場合
+                inv_mat.values[j][i] = cofnnm.cofactor();
+            } else {
+                // 奇数の場合
+                inv_mat.values[j][i] = -cofnnm.cofactor();
+            }
+        }
+    }
+    
+    // 余因子の逆数を求める
+    const float rep_cof = 1.f / cof;
+    
+    // スカラーの乗算
+    inv_mat *= rep_cof;
+    
+    return true;
+}
+
+/**
+ * 標準出力に行列を表示する
+ */
+template<typename TYPE, int COLS, int ROWS>
+void Matrix<TYPE, COLS, ROWS>::disp() const {
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            std::cout << values[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+/**
+ * 余因子行列を返却する
+ */
+template<typename TYPE, int COLS, int ROWS>
+void Matrix<TYPE, COLS, ROWS>::cofNNMatrix(Matrix<TYPE, COLS-1, ROWS-1>& ret, int x, int y) const {
+    for (int i = 0, ii = 0; i < ret.rows(); i++, ii++) {
+        if (ii == y) {
+            ii++;
+        }
+        
+        for (int j = 0, jj = 0; j < ret.cols(); j++, jj++) {
+            if (jj == x) {
+                jj++;
+            }
+            ret.values[i][j] = values[ii][jj];
+        }
+    }
+}
+
+/**
+ * 余因子を返却する
+ */
+template<typename TYPE, int COLS, int ROWS>
+TYPE Matrix<TYPE, COLS, ROWS>::cofactor() const {
+    constexpr int cols = COLS == 2 ? 1 : COLS;
+    
+    TYPE sum = 0;
+    for (int k = 0; k < cols; k++) {
+        
+        TYPE sum_p = 1;
+        TYPE sum_m = 1;
+        for (int i = 0; i < COLS; i++) {
+            int i0 = i;
+            int j0 = (i + k) % ROWS;
+            sum_p *= values[i0][j0];
+            
+            int i1 = i;
+            int j1 = ((COLS - 1)+ k - i) % COLS;
+            sum_m *= values[i1][j1];
+        }
+        sum += sum_p;
+        sum -= sum_m;
+    }
+    
+    return sum;
+}
+
+/**
+ * スカラーの乗算
+ */
+template<typename TYPE, int COLS, int ROWS>
+const Matrix<TYPE, COLS, ROWS>& Matrix<TYPE, COLS, ROWS>::operator *= (float k) {
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            values[i][j] *= k;
+        }
+    }
+    return *this;
+}
